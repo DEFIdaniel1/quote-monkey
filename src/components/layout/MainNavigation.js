33eb1e02b3a057948bf11203c4b55aba1c45125a
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-05-05 20:50:47 -0600

    built and deployed to firebase

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -6,7 +6,7 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>Super Quotes!</div>
+      <div className={classes.logo}>Quote Monkey</div>
       <nav className={classes.nav}>
         <ul>
           <li>
